f96cc13733c1a7639b31883fadf45b3dc13f6fce
rishavmahapatra <rishavmahapatra24@gmail.com>
2026-01-20 06:22:19 +0530

    url changed

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,2 +1,2 @@
-// export const url = "https://grox-ai-backend.onrender.com";
-export const url = "http://localhost:8000";
+export const url = "https://grox-ai-backend.onrender.com";
+// export const url = "http://localhost:8000";
